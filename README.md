996c88592df51b984d65b6e6f28249d19e03d796
Nikos Katzouris <nikos.gatzouris@gmail.com>
2020-01-28 00:19:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Online Relational Leaning
+
+Refactored, latest version of OLED & WOLED plus more tools for for online logical & relational learning. 
+
+## Installation
+
+You'll need to have Scala 2.12 with SBT (Scala Build Tool), JDK 8, and Python 2.7 installed on your machine.
+
+* Clone or download the source code:
+  * `git clone https://github.com/nkatzz/OLED.git`
+* `cd OLED/install-scripts`
+* `./install.sh`
+* Update your `PATH`, `PYTHONPATH` and `LD_LIBRARY_PATH`. Append the following lines to your `~/.profile` (or `~/.bashrc`) file:
+  * `LD_LIBRARY_PATH=<PATH TO CLONED SOURCE>/OLED/external_dependencies/lpsolve55:$LD_LIBRARY_PATH`  
+  * `export LD_LIBRARY_PATH`
+  * `export PATH=$PATH:<PATH TO CLONED SOURCE>/OLED/external_dependencies/LoMRF/target/universal/LoMRF-0.5.5-SNAPSHOT/bin:<PATH TO CLONED SOURCE>/OLED/external_dependencies/clingo/clingo-4.5.4-source/build/release`
+  * `export PYTHONPATH=$PYTHONPATH:<PATH TO CLONED SOURCE>/OLED/external_dependencies/clingo/clingo-4.5.4-source/build/release/python`
+
+## Data
+
+Detailed instructions on how to perform a test run with ``OLED`` are provided in the manual. Please refer to the manual for details on the data and the learning task we'll use for this test run. In sort:
+
+* Install MongoDB.
+* Download some data and some background knowledge at some location: 
+   * `wget http://users.iit.demokritos.gr/~nkatz/oled/caviar-data.zip`
+   * `wget http://users.iit.demokritos.gr/~nkatz/oled/bk.zip`
+* `unzip caviar-data.zip`
+* `unzip bk.zip`
+* `cd caviar-data`
+* Import the data into Mongo:
+   * `mongoimport --db caviar-train --collection examples --file caviar-train.json`
+   * `mongoimport --db caviar-test --collection examples --file caviar-test.json`
+* Make sure everything is ok (after axecuting the `show dbs` command you should see the newly-created dbs 'caviar-train' and 'caviar test'):
+   * `mongo`
+   * `show dbs`
---
+++ project/Dependency.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2016  Nikos Katzouris
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import sbt._
+
+object Dependency {
+
+  object v {
+    //final val Akka = "2.5.17"
+    final val Akka = "2.5.6"
+    final val ScalaLogging = "3.9.2"
+    final val Logback = "1.2.3"
+    final val MongoDB = "3.1.1"
+    final val ScalaTest = "3.0.5"
+    final val ScalaZ = "7.2.29"
+    final val SizeOf = "0.1"
+    final val Parboiled = "2.1.8"
+    final val Optimus = "3.2.0"
+    final val LoMRF = "1.0.0-SNAPSHOT"
+    //final val Logicutils = "0.1-SNAPSHOT"
+  }
+
+  // Akka.io
+  lazy val Akka = "com.typesafe.akka" %% "akka-actor" % v.Akka
+
+  // Logging using SLF4J and logback
+  lazy val Logging = Seq(
+    "com.typesafe.scala-logging" %% "scala-logging" % v.ScalaLogging,
+    "ch.qos.logback" % "logback-classic" % v.Logback
+  )
+
+  // MongoDB (update to "org.mongodb.scala" %% "mongo-scala-driver" % "2.1.0")
+  lazy val MongoDB = "org.mongodb" %% "casbah" % v.MongoDB
+
+  // ScalaTest for UNIT testing
+  lazy val ScalaTest = "org.scalatest" %% "scalatest" % v.ScalaTest % "test"
+
+  // Tools
+  lazy val Tools = Seq(
+    "org.scalaz" %% "scalaz-core" % v.ScalaZ,
+    "com.madhukaraphatak" % "java-sizeof_2.11" % v.SizeOf,
+    "org.parboiled" %% "parboiled" % v.Parboiled,
+    "com.github.vagmcs" %% "scalatikz" % "0.4.4"
+  )
+
+  // Optimus library for linear and quadratic optimization
+  lazy val Optimus = Seq(
+    "com.github.vagmcs" %% "optimus" % v.Optimus,
+    "com.github.vagmcs" %% "optimus-solver-lp" % v.Optimus
+  )
+
+  // LoMRF library for Markov Logic Networks
+  lazy val LoMRF = "com.github.anskarl" %% "lomrf" % v.LoMRF
+
+  //lazy val LogicUtils = "com.github.nkatzz" %% "logicutils" % v.Logicutils
+
+  //lazy val vegas = "org.vegas-viz" %% "vegas" % "0.3.12" // plotting library
+}
